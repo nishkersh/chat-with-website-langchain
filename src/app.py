e0d9aed911fb01855bbdc2edc30aaaaa2c99c57d
nishkersh <reekc11@gmail.com>
2024-03-28 22:32:07 +0530

    added

--- src/app.py
+++ src/app.py
@@ -11,30 +11,52 @@ def get_response(user_input):
 
 # app config
 st.set_page_config(page_title="Chat With Websites",page_icon="🤖")
-
 st.title("Chat With Websites")
-chat_history = [
-    AIMessage(content="Hello, I am a bot, How can I help you ? ")
 
-]
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I am a bot, How can I help you ? ")
+
+    ]
 
 # sidebar
 with st.sidebar:
     st.header("settings")
     website_URL=st.text_input("Website URL")
 
+#  disable conversion until website url is not given 
+
+if website_URL is None or website_URL == "":
+    st.info("Please enter a website URL ")
+else:
+    # user input 
+    user_query=st.chat_input("Type Your Message Here...")
+    if user_query is not None and user_query !="":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
 
 
-# user input 
-user_query=st.chat_input("Type Your Message Here...")
-if user_query is not None and user_query !="":
-    response = get_response(user_query)
+    #  conversion
+            
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
 
-    with st.chat_message("Human"):
-        st.write(user_query)
 
-    with st.chat_message("AI"):
-        st.write(response)
+
+        # with st.chat_message("Human"):
+        #     st.write(user_query)
+
+        # with st.chat_message("AI"):
+        #     st.write(response)
+
+        # with st.sidebar:
+        #     st.write(st.session_state.chat_history)
 
 
 
